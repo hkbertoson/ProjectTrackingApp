5ee3bf7ac7367e5353f94d01b2a31cad32fcaeac
Hunter Bertoson <Hunterkylebertoson@gmail.com>
2022-06-30 14:50:49 -0500

    Database Connection Created

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
+const colors = require('colors');
 const express = require('express');
+const connectDB = require('../config/db');
 require('dotenv').config();
-
-const { graphqlHTTP } = require('express-graphql');
+const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: process.env.NODE_ENV === 'development',
-}));
+// Connect to Database
+connectDB();
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: process.env.NODE_ENV === 'development',
+	})
+);
 
 app.listen(port, console.log(`Application running on ${port}`));
