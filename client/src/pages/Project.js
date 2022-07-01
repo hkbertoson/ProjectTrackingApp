68c7d67b3bb6aed324108e53122a5f7754120609
Hunter Bertoson <Hunterkylebertoson@gmail.com>
2022-07-01 16:01:34 -0500

    V1.0.0

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -1,6 +1,8 @@
 import {Link, useParams} from 'react-router-dom';
 import Spinner from '../components/Spinner';
 import ClientInfo from '../components/ClientInfo';
+import DeleteProjectButton from '../components/DeleteProjectButton';
+import EditProjectForm from '../components/EditProjectForm';
 import {useQuery} from '@apollo/client';
 import {GET_PROJECT} from '../queries/projectQueries';
 
@@ -25,6 +27,9 @@ export default function Project() {
 					<p className="lead">{data.project.status}</p>
 
 					<ClientInfo client={data.project.client} />
+
+					<DeleteProjectButton projectId={data.project.id} />
+					<EditProjectForm project={data.project} />
 				</div>
 			)}
 		</>
