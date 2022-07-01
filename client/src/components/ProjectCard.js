309eb2dddd69cf524d89635ef24539753cfa3987
Hunter Bertoson <Hunterkylebertoson@gmail.com>
2022-07-01 15:09:46 -0500

    Updated Schema and created project page

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -5,7 +5,9 @@ export default function ProjectCard({project}) {
 				<div className="card-body">
 					<div className="d-flex justify-content-between align-items-center">
 						<h5 className="card-title">{project.name}</h5>
-						<a href={`/projects/${project.id}`} className="btn btn-white"></a>
+						<a href={`/projects/${project.id}`} className="btn btn-light">
+							View
+						</a>
 					</div>
 					<p className="small">
 						Status: <strong>{project.status}</strong>
